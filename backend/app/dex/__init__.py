794a0b68382f7a96801a2d91101686283239d8c4
andrewbyteforge <andrewbyteforge@gmail.com>
2025-08-17 10:42:42 +0100

    u

---
+++ backend/app/dex/__init__.py
@@ -0,0 +1,6 @@
+"""
+DEX adapters package for multi-chain quote aggregation.
+"""
+
+# This file makes the dex directory a Python package
+# No additional code needed - imports are handled in individual adapter files
